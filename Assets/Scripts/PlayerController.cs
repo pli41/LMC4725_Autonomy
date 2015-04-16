ae2606959a7b914d9114648cc3ecad044a39b914
YeeHawGandalf <mpalmer38@gatech.edu>
2015-04-15 23:19:40 -0400

    Hold up Achievement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,21 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour {
 	public int playerNum;
 	public GameObject topLimit, botLimit;
-
+	
 	public GameObject fire;
 	public bool onFire;
-
+	
 	public int currentScore;
 	public GUIText scoreText;
-
+	
 	public KeyCode upKey, downKey;
 	private bool CanHitTopWall, CanHitBottomWall;
-
+	
 	public Statistics statistics;
 	public List<Achievement> achievements = new List<Achievement>();
-
+	
 	public float progress;
-
+	
 	// Use this for initialization
 	void Start () {
 		Reset();
@@ -31,20 +31,20 @@ public class PlayerController : MonoBehaviour {
 		if(currentScore > 22){
 			onFire = true;
 		}
-
+		
 		if(onFire){
 			fire.GetComponent<ParticleSystem>().Play();
 		}
 		else{
 			fire.GetComponent<ParticleSystem>().Pause();
 		}
-
+		
 		if(Input.GetKey(upKey)){
 			if(transform.position.y <= topLimit.transform.position.y){
 				Vector3 currentPos = transform.position;
 				Vector3 newPos = new Vector3(currentPos.x, currentPos.y+0.3f, currentPos.z);
 				transform.position = newPos;
-
+				
 				statistics.moveUpAmount += 0.3f;
 			}
 			else
@@ -55,19 +55,19 @@ public class PlayerController : MonoBehaviour {
 					CanHitTopWall = false;
 				}
 			}
-
-			statistics.holdDownDuration += Time.deltaTime;
+			
+			statistics.holdUpDuration += Time.deltaTime;
 			CanHitBottomWall = true;
 		} else {
-			statistics.holdDownDuration = 0f;
+			statistics.holdUpDuration = 0f;
 		}
-
+		
 		if(Input.GetKey(downKey)){
 			if(transform.position.y >= botLimit.transform.position.y){
 				Vector3 currentPos = transform.position;
 				Vector3 newPos = new Vector3(currentPos.x, currentPos.y-0.3f, currentPos.z);
 				transform.position = newPos;
-
+				
 				statistics.moveDownAmount += 0.3f;
 			}
 			else
@@ -78,37 +78,45 @@ public class PlayerController : MonoBehaviour {
 					CanHitBottomWall = false;
 				}
 			}
-
-			statistics.holdUpDuration += Time.deltaTime;
+			
+			statistics.holdDownDuration += Time.deltaTime;
 			CanHitTopWall = true;
 		} else {
-			statistics.holdUpDuration = 0f;
+			statistics.holdDownDuration = 0f;
 		}
-
-		progress = achievements[0].Progress();
+		
+		progress = achievements[1].Progress();
 	}
-
+	
 	public void ScorePoint()
 	{
 		currentScore++;
 		scoreText.text = currentScore.ToString();
 	}
-
+	
 	public void Reset(){
 		onFire = false;
 		currentScore = 0;
 	}
-
+	
 	public void CreateAchievements()
 	{
 		Achievement HitBallAchievement = new Achievement("Hit Ball 10 Times", "Wow, you hit the ball ten times that's really cool buddy.");
 		HitBallAchievement.progress = HitBallAchievementProgress;
-
 		achievements.Add(HitBallAchievement);
+		
+		Achievement HoldUpAchievement = new Achievement("title", "description");
+		HoldUpAchievement.progress = HoldUpAchievementProgress;
+		achievements.Add(HoldUpAchievement);
 	}
-
+	
 	public float HitBallAchievementProgress()
 	{
 		return (statistics.hitBallCount / 10f);
+	}
+	
+	public float HoldUpAchievementProgress()
+	{
+		return (statistics.GetHoldUpDuration() / 20f);
 	}
 }
