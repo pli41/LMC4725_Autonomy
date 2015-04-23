ecd21f9e0c39de34530fd3f2d560c342a300166e
Peilin Li <lipeilin@gatech.edu>
2015-04-23 15:40:29 -0400

    UI for a single achievement is done.
    You can find the panel for a single achievement in the Canvas now, and
    the testSlider game object contains the script which serves as a
    example of changing the description and slider value.

---
+++ Assets/Scripts/TestSlider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class TestSlider : MonoBehaviour {
+
+	public Slider slider;
+	public Text text;
+	public float value;
+	public string desc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = value;
+		text.text = desc;
+	}
+}
